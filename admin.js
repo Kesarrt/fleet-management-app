415fbab70f4c661178b6091ef9e77b46119dc05b
Kesarrt <tandulkarkhushi@gmail.com>
2025-11-29 20:27:30 +0530

    incomplete

---
+++ admin.js
@@ -0,0 +1,16 @@
+let fleet=[];
+const $=(id)=>
+    document.getElementById(id);
+
+const render=()=>{
+    const c=$('filterCategory').value, s=$('filterAvailability').value;
+
+
+    hides DataTransferItemList,Map generates HTML 
+    $('fleetContainer').innerHTML=fleet
+        .map((v,i)=>(c==="All"|| XMLDocument.v.cat===c)&& (s==="All" || v.availability === s)?`<div class="card">
+            <img src="${v.img}" alt="">
+            <h3>${v.name}</h3>
+            <p>Price: ${v.price} </p>
+            <button onclick="rent(${i})">Rent</button>
+        </div>`:""}
